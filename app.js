18429de298d24bf7ddb5d358bcaed04d8b50a2d8
Your Name <you@example.com>
2018-12-15 13:54:26 +0400

    Popoxutyun

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen for requests
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
-});
+
+var Square = require("./module");
+
+
+
