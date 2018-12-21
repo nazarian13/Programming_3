65a7dffde0bbf6839fb8b6bfaf3c01dca1d6c27c
Your Name <you@example.com>
2018-12-21 17:22:59 +0400

    popxutyn

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var messages = [];
 
-app.use(express.static("."));
+app.use(express.static("./public"));
 app.get('/', function (req, res) {
     res.redirect('index.html');
 });
@@ -12,8 +11,36 @@ server.listen(3000);
 
 var matrix = require("./modules/matrix");
 console.log(matrix);
+
 io.on('connection', function (socket) {
     socket.emit("firstMatrix", matrix);
 });
 
+setInterval(draw, time);
+
+var framecount = 5;
+
+var time = 1000/framecount;
+
+function draw() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x].index == 1) {
+                matrix[y][x].mul(matrix);
+            }
+            else if (matrix[y][x].index == 2) {
+                matrix[y][x].eat();
+            }
+            else if (matrix[y][x].index == 3) {
+                matrix[y][x].eat();
+            }
+            else if (matrix[y][x].index == 4) {
+                matrix[y][x].eat();
+            }
+            else if (matrix[y][x].index == 5) {
+                matrix[y][x].eat();
+            }
+        }
+    }
+}
 
--- modules/matrix.js
+++ modules/matrix.js
@@ -2,20 +2,23 @@ var matrix = [];
 var n = 80, m = 80;
 var side = 50;
 
-var Grass = require("./class.grass");
-var GrassEater = require ("./class.grasseater");
-var Depredador = require("./class.depredador");
-var Hunter = require("./class.hunter");
-var Destroyer = require("./class.destroyer");
-
 function setup() {
 
     for (var y = 0; y < m; y++) {
-        matrix[y] = [];
+        matrix[y] = [x];
         for (var x = 0; x < n; x++) {
             matrix[y][x] = random([0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 1, 0, 3, 1, 1, 0, 0, 0, 1, 1, 0, 0, 4, 0, 0, 1, 1, 0, 0, 5, 0, 0]);
         }
     }
+
+var Grass = require("./class.grass.js");
+var GrassEater = require ("./class.grasseater.js");
+var Depredador = require("./class.depredador.js");
+var Hunter = require("./class.hunter.js");
+var Destroyer = require("./class.destroyer.js");
+
+
+    function draw (){
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
             if (matrix[y][x] == 1) {
@@ -36,3 +39,6 @@ function setup() {
         }
     }
 }
+}
+
+module.exports = matrix;
